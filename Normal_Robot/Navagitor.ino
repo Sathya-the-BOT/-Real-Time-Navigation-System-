54fa450e4507771f3de5d86ce63033a0b52a07e5
Sathya Swaroop M S <sathyaswaroop02@gmail.com>
2025-09-09 22:43:04 +0530

    Initial Commits

---
+++ Normal_Robot/Navagitor.ino
@@ -0,0 +1,107 @@
+//Project-1: //ARDUINO LINE FOLLOWING CAR - QUAD Robotics - A unit of Quad Store//
+//www.quadstore.in
+
+// YOU HAVE TO INSTALL THE AFMOTOR LIBRARY BEFORE UPLOAD THE CODE//
+// GO TO SKETCH >> INCLUDE LIBRARY >> ADD .ZIP LIBRARY >> SELECT AF MOTOR ZIP FILE //
+
+
+//including the libraries
+#include <AFMotor.h>
+
+//defining pins and variables
+#define lefts A0
+#define rights A1
+
+const int trigpin = A4;
+const int echopin = A5;
+long duration;
+int distance;
+
+//defining motors
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+
+
+void setup() {
+  //Setting the motor speed
+  motor1.setSpeed(225);
+  motor2.setSpeed(225);
+  motor3.setSpeed(225);
+  motor4.setSpeed(225);
+  //Declaring PIN input types
+  pinMode(lefts,INPUT);
+  pinMode(rights,INPUT);
+  
+  pinMode(trigpin , OUTPUT);
+  pinMode(echopin , INPUT);
+  //Begin serial communication
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+  //Printing values of the sensors to the serial monitor
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+  duration = pulseIn(echopin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.println("Distance: ");
+  Serial.println(distance);
+
+  Serial.println(analogRead(lefts));
+  Serial.println(analogRead(rights));
+  int left = analogRead(lefts);
+  int right = analogRead(rights);
+  //line detected by both
+  if(distance < 10) 
+  {
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+
+  } 
+  else
+  {
+    if((left)<=40 && (right)<=40){
+      //Forward
+      motor1.run(FORWARD);
+      motor2.run(FORWARD);
+      motor3.run(FORWARD);
+      motor4.run(FORWARD);
+    }
+    //line detected by left sensor
+    else if((left)<=40 && !(right)<=40){
+      //turn left
+      motor1.run(FORWARD);
+      motor2.run(FORWARD);
+      motor3.run(BACKWARD);
+      motor4.run(BACKWARD);
+      
+    }
+    //line detected by right sensor
+    else if(!(left)<=40 && (right)<=40){
+      //turn right
+      motor1.run(BACKWARD);
+      motor2.run(BACKWARD);
+      motor3.run(FORWARD);
+      motor4.run(FORWARD);
+    
+    }
+    //line detected by none
+    else if(!(left)<=40 && !(right)<=40){
+      //stop
+      motor1.run(RELEASE);
+      motor2.run(RELEASE);
+      motor3.run(RELEASE);
+      motor4.run(RELEASE);
+    
+    }
+  }
+}
---
+++ Robot_with_UltraSoic_Sensor/UltraSonic_Navigator.ino
@@ -0,0 +1,228 @@
+#include <AFMotor.h>
+#include <NewPing.h>
+#include <Servo.h>
+
+#define TRIG_PIN A4 
+#define ECHO_PIN A5
+#define trigpin A4 
+#define echopin A5
+#define lefts A0
+#define rights A1
+#define MAX_DISTANCE 100
+#define MAX_SPEED 255 // sets speed of DC motors
+#define MAX_SPEED_OFFSET 20
+
+long duration;
+int distance_ir;
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ); 
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+Servo myservo;
+boolean goesForward=false;
+
+//int distance = 100;
+int speedSet = 0;
+
+void setup() 
+{
+  myservo.attach(9); 
+  myservo.write(115); 
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
+
+  pinMode(lefts,INPUT);
+  pinMode(rights,INPUT);
+
+  pinMode(trigpin , OUTPUT);
+  pinMode(echopin , INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() 
+{
+ int distanceR = 0;
+ int distanceL = 0;
+ delay(40);
+  analogWrite(trigpin, LOW);
+  delayMicroseconds(10);
+  analogWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  analogWrite(trigpin, LOW);
+  duration = pulseIn(echopin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.println("Distance:");
+  Serial.println(distance);
+
+  Serial.println(analogRead(lefts));
+  Serial.println(analogRead(rights));
+ 
+ if(distance<=30)
+ {
+   moveStop();
+   delay(100);
+   moveBackward();
+   delay(300);
+   moveStop();
+   delay(200);
+   distanceR = lookRight();
+   delay(200);
+   distanceL = lookLeft();
+   delay(200);
+
+  if(distanceR>=distanceL)
+    {
+      turnRight();
+      moveStop();
+    }
+  else
+    {
+      turnLeft();
+      moveStop();
+    }
+  }
+  else
+  {
+   moveForward();
+  }
+  distance = readPing();
+}
+
+int lookRight()
+{
+  myservo.write(50); 
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  myservo.write(115); 
+  return distance;
+}
+
+int lookLeft()
+{
+  myservo.write(170); 
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  myservo.write(115); 
+  return distance;
+  delay(100);
+}
+
+int readPing() 
+{ 
+ delay(70);
+ int cm = sonar.ping_cm();
+ if(cm==0)
+ {
+  cm = 250;
+ }
+ return cm;
+}
+
+void moveStop() 
+{
+  motor1.run(RELEASE); 
+  motor2.run(RELEASE);
+  motor3.run(RELEASE); 
+  motor4.run(RELEASE);
+} 
+ 
+void moveForward() 
+{
+  if(!goesForward)
+  {
+    goesForward=true;
+    motor1.run(FORWARD); 
+    motor2.run(FORWARD); 
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD); 
+    for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) 
+    {
+      motor1.setSpeed(speedSet);
+      motor2.setSpeed(speedSet+MAX_SPEED_OFFSET);
+      motor3.setSpeed(speedSet);
+      motor4.setSpeed(speedSet+MAX_SPEED_OFFSET);
+      delay(5);
+    }
+  }
+}
+
+void moveBackward() 
+{
+  goesForward=false;
+  motor1.run(BACKWARD); 
+  motor2.run(BACKWARD); 
+  motor3.run(BACKWARD); 
+  motor4.run(BACKWARD); 
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet+MAX_SPEED_OFFSET);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet+MAX_SPEED_OFFSET);
+    delay(5);
+  }
+}
+
+void turnRight() 
+{
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD); 
+  motor3.run(FORWARD);
+  motor4.run(FORWARD); 
+  delay(300);
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD); 
+  motor3.run(FORWARD);
+  motor4.run(FORWARD); 
+  delay(300);
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD); 
+  motor3.run(FORWARD);
+  motor4.run(FORWARD); 
+  delay(300);
+  motor1.run(FORWARD); 
+  motor2.run(FORWARD); 
+  motor3.run(FORWARD); 
+  motor4.run(FORWARD); 
+} 
+ 
+void turnLeft() 
+{
+  motor1.run(FORWARD); 
+  motor2.run(FORWARD); 
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD); 
+  delay(300);
+  motor1.run(FORWARD); 
+  motor2.run(FORWARD); 
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD); 
+  delay(300);
+  motor1.run(FORWARD); 
+  motor2.run(FORWARD); 
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD); 
+  delay(300);
+  motor1.run(FORWARD); 
+  motor2.run(FORWARD);
+  motor3.run(FORWARD); 
+  motor4.run(FORWARD);
+}
